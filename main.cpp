f57e49f46094762d88ee15c9db9b1563a5d9e3ba
nerdic <nerdic@pop-os.localdomain>
2021-06-16 15:14:30 +0700

    finally added functions

--- main.cpp
+++ main.cpp
@@ -5,18 +5,61 @@
 
 //TODO Create seperate functions for cleaner code
 
+void determineWinner(int, int);
+int getPlayerChoice(int);
+int getComputerChoice(int);
+
 int main()
 {
-  // initialize seed generator
-  srand (time(NULL));
-
+  int computerChoice;
   int playerChoice;
-  int computerChoice = rand() % 3 + 1;
 
   char playAgain = 'y';
 
   while(playAgain == 'y')
   {
+    playerChoice = getPlayerChoice(playerChoice);
+    computerChoice = getComputerChoice(computerChoice);
+    determineWinner(playerChoice, computerChoice);
+
+    std::cout << "Do you want to play again? y/n\n";
+    std::cin >> playAgain;
+    
+    if (playAgain == 'n')
+    {
+      break;
+    }
+  }
+
+  return 0;
+
+}
+
+void determineWinner(int playerChoice, int computerChoice)
+{
+    if (playerChoice == computerChoice)
+    {
+      std::cout << "Draw!\n";
+    }
+    else if (playerChoice == 1 && computerChoice ==  3)
+    {
+      std::cout << "You win!\n";
+    }
+    else if (playerChoice == 2 && computerChoice == 1)
+    {
+      std::cout << "You win!\n";
+    }
+    else if (playerChoice == 3 && computerChoice == 2)
+    {
+      std::cout << "You win!\n";
+    }
+    else {
+      std::cout << "Computer Wins!\n";
+    }
+}
+
+int getPlayerChoice(int playerChoice)
+{
     std::cout << "\tWelcome to Rock, Paper Scissors!\n\n";
     std::cout << "---Game Menu---\n";
     std::cout << "1) Rock\n";
@@ -41,53 +84,31 @@ int main()
       default:
         std::cout << "Invalid Option\n\n";
   }
+  return playerChoice;
+}
 
-    std::cout << "Computer chose:\n\n";
-    
-    switch(computerChoice)
-    {
-      case 1:
-        std::cout << "Rock\n";
-        break;
-      case 2: // paper
-        std::cout << "Paper\n";
-        break;
-      case 3: // scissors
-        std::cout << "Scissors\n";
-        break;
-      default:
-        std::cout << "Invalid Option\n";
-  }
+int getComputerChoice(int computerChoice)
+{
+  // initialize seed generator
+  srand (time(NULL));
+  computerChoice = rand() % 3 + 1;
 
-    if (playerChoice == computerChoice)
-    {
-      std::cout << "Draw!\n";
-    }
-    else if (playerChoice == 1 && computerChoice ==  3)
-    {
-      std::cout << "You win!\n";
-    }
-    else if (playerChoice == 2 && computerChoice == 1)
-    {
-      std::cout << "You win!\n";
-    }
-    else if (playerChoice == 3 && computerChoice == 2)
-    {
-      std::cout << "You win!\n";
-    }
-    else {
-      std::cout << "Computer Wins!\n";
-    }
+  std::cout << "Computer chose:\n";
 
-    std::cout << "Do you want to play again? y/n\n";
-    std::cin >> playAgain;
-
-    if (playAgain == 'n')
-    {
+  switch(computerChoice)
+  {
+    case 1:
+      std::cout << "Rock\n\n";
       break;
-    }
+    case 2: // paper
+      std::cout << "Paper\n\n";
+      break;
+    case 3: // scissors
+      std::cout << "Scissors\n\n";
+      break;
+    default:
+      std::cout << "Invalid Option\n\n";
   }
-
-  return 0;
+  return computerChoice;
 
 }
